6cb828f9f65e5c614b75fd86c05302a81e55a9fe
BluCypher <github.blucypher@icloud.com>
2016-09-15 22:55:18 -0400

    Header update

--- Functions/Get-InstalledSoftware.ps1
+++ Functions/Get-InstalledSoftware.ps1
@@ -1,6 +1,14 @@
+﻿<#
 	.NOTES
 	===========================================================================
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.119
+	 Filename:     	Get-InstalledSoftware.ps1
+	 Created on:   	2016-09-14 21:51
+	 Created by:   	Mike Sims
+	 Changed on:   	2016-09-15
+	 Changed by:   	Mike Sims
+	 Version:     	1.0
+	 History:      	1.0 - Initial Release - No known bugs
 	===========================================================================
 	.DESCRIPTION
 		Get-InstalledSoftware Function
